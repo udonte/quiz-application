d968b53c7b32e4eef2c142281c8e6e9129f4bdcc
George Udonte <georgeudonte@gmail.com>
2023-11-22 15:56:56 +0100

    added changes in html and js file

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const correctedAnswers = ["A", "A", "A", "A", "A", "A"];
-const result = document.querySelector(".result");
+const correctAnswers = ["B", "A", "A", "B", "D", "B"];
+let score = 0;
 const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
 const questions = document.querySelectorAll(".question");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let score = 0;
   const userAnswers = [
     form.q1.value,
     form.q2.value,
@@ -13,18 +13,18 @@ form.addEventListener("submit", (e) => {
     form.q5.value,
     form.q6.value,
   ];
-  correctedAnswers.forEach((answer, index) => {
-    if (answer === userAnswers[index]) {
+  userAnswers.forEach((userAnswer, index) => {
+    if (userAnswer === correctAnswers[index]) {
       score += 1;
       questions[index].classList.add("correct");
     } else {
       questions[index].classList.add("wrong");
     }
   });
-  console.log(score);
   scroll(0, 0);
   result.classList.remove("hide");
   result.querySelector(
     "p"
-  ).textContent = `You scored ${score}/${questions.length}!`;
+  ).textContent = `Your score is ${score}/ ${correctAnswers.length}`;
+  console.log(score);
 });
